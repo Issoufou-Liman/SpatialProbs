9a367304da10c0167ecafd23a22795f35fcf1d21
Issoufou-Liman <issoufoul@gmail.com>
2019-09-13 23:09:16 +0300

    Adding names to different combinaisons of fuzzy quantifiers in reclassification_matrix function

--- R/reclassifion_matrix.R
+++ R/reclassifion_matrix.R
@@ -17,7 +17,7 @@
 #' reclassifion_matrix (sample)
 #' @importFrom raster canProcessInMemory getValues sampleRandom
 #' @export
-reclassifion_matrix <- function(x, split_IQR = FALSE, na.rm = TRUE , type = 5, ..., custum_rclmat=NULL,
+reclassifion_matrix <- function(x, split_IQR = FALSE, custum_rclmat=NULL,
                                 sample = FALSE, size = 1000){
   if (!(class(x) %in% c("integer","numeric", 'matrix', 'data.frame', "RasterLayer", 'RasterBrick', 'RasterStack'))){
     stop(paste(deparse(substitute(xx)), "must be integer, numeric, matrix, data.frame, RasterLayer, RasterBrick, RasterStack"))
@@ -31,28 +31,38 @@ reclassifion_matrix <- function(x, split_IQR = FALSE, na.rm = TRUE , type = 5, .
     }
   }
 
-  fun <- function(x, split_IQR, na.rm , type, ...,custum_rclmat){
+  fun <- function(x, split_IQR , custum_rclmat){
     if(!is.null(custum_rclmat)){
-      rclmat <- unique(custum_rclmat)
+      # rclmat <- unique(custum_rclmat)
+      rclmat <- custum_rclmat[!duplicated(custum_rclmat)]
+      if(is.null(names(rclmat))){
+        names(rclmat) <- paste0('x', "_", 1:length(rclmat))
+      }
     } else {
-      rclmat <- get_boxplot_range_1d(x, split_IQR = split_IQR, na.rm = na.rm , type = type, ...)
-      rclmat <- unique(rclmat)
+      rclmat <- get_boxplot_range_1d(x, split_IQR = split_IQR)
+      # rclmat <- unique(rclmat)
+      rclmat <- rclmat[!duplicated(rclmat)]
     }
 
     if (length(rclmat) <= 2){
       stop(paste('only', length(rclmat), "elemnts in the reclassification matrix."))
     }
+    rownams <- lapply(1:(length(names(rclmat))-1), function(i){ # constructing names for different combinaison of lower and upper bounds
+      paste0(names(rclmat)[i], '_', names(rclmat)[i+1])
+    })
     rclmat <- sort(c(rclmat, rclmat[2:(length(rclmat)-1)]), decreasing = FALSE)
     rclmat[1] <- (-Inf); rclmat[length(rclmat)] <- Inf
+
     rclmat <- matrix(rclmat, ncol = 2, byrow = TRUE)
     rclmat <- cbind(rclmat, 1:nrow(rclmat))
+    rownames(rclmat) <- rownams
     return(rclmat)
   }
   if (class(x) %in% c('matrix', 'data.frame')){
     sapply(X=colnames(x), function(i){
-      fun (x=x[, i], split_IQR = split_IQR, na.rm = na.rm , type=type, custum_rclmat = custum_rclmat)
+      fun (x=x[, i], split_IQR = split_IQR, custum_rclmat = custum_rclmat)
     }, simplify = FALSE, USE.NAMES = TRUE)
   } else {
-    fun (x=x, split_IQR = split_IQR, na.rm = na.rm , type=type, custum_rclmat = custum_rclmat)
+    fun (x=x, split_IQR = split_IQR, custum_rclmat = custum_rclmat)
   }
 }
